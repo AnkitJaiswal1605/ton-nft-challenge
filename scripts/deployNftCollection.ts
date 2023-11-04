f83ab1201f33505c8fcb6e65338b8eb2dc95f628
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2023-11-04 17:02:31 +0530

    VanshulAgarwal all challenge except last

--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -4,7 +4,7 @@ import { NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
     const OFFCHAIN_CONTENT_PREFIX = 0x01;
-    const metadata_link = "https://ivory-peaceful-xerinae-817.mypinata.cloud/ipfs/QmQfx8GsreZkQ3eXpa8MMZoYX6kYRoGgcp45bbiPUTmnki/"
+    const metadata_link = "https://gateway.pinata.cloud/ipfs/QmaNuDRDgCM8vDUMEpCmCaJxdbL21uJnq2Dh32T13ANmfQ"
 
     let content = beginCell().storeInt(OFFCHAIN_CONTENT_PREFIX, 8).storeStringRefTail(metadata_link).endCell();
 
